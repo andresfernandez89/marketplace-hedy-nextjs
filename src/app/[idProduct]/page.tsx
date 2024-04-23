0785219b342911093a529b6ff993966b703652a8
andresfernandez89 <andresfernandez.mdp@gmail.com>
2024-04-23 11:28:00 -0300

    rating component

--- src/app/[idProduct]/page.tsx
+++ src/app/[idProduct]/page.tsx
@@ -9,9 +9,10 @@ import Image from "next/image";
 import { getProductById } from "@/lib/api";
 import { capitalizeFirstLetter } from "@/lib/utils";
 import Counter from "@/components/counter";
+import Rating from "@/components/rating";
 
 export default async function ProductId() {
-  const product = await getProductById(5);
+  const product = await getProductById(2);
   return (
     <Card className="mx-4 my-10 md:mx-auto md:flex md:max-w-4xl ">
       <div className="relative bg-[#fefefe]  max-md:rounded-t-lg md:max-h-80 md:w-full md:rounded-l-lg ">
@@ -29,27 +30,7 @@ export default async function ProductId() {
       </div>
       <CardContent className="flex-col max-md:pt-6 md:flex md:justify-evenly md:py-1 md:pl-3 md:pr-1">
         <CardHeader className="p-0 md:py-1">{product.title}</CardHeader>
-        <div className="flex flex-row font-normal text-gray-700 dark:text-gray-400">
-          <p className="mr-1 text-base">{product.rating.rate}</p>
-          <div className="flex items-center">
-            {[1, 2, 3, 4, 5].map((star) => (
-              <svg
-                key={star}
-                className={`ms-1 h-4 w-4 ${
-                  product.rating.rate > star
-                    ? "text-[#3b82f6]"
-                    : "stroke-[#94a3b8] text-[#020817]"
-                }`}
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-            ))}
-          </div>
-        </div>
+        <Rating rate={product.rating.rate} />
         <p className="mb-3 text-sm font-light text-blue-100">
           {capitalizeFirstLetter(product.category)}
         </p>
