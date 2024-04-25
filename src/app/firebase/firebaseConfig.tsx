9aa69075ac21c0ee41c1467a00e39a77a78b461a
BrianTahiel <briang.2012@hotmail.com>
2024-04-25 17:17:02 -0300

    firebase config

--- src/app/firebase/firebaseConfig.tsx
+++ src/app/firebase/firebaseConfig.tsx
@@ -1,6 +1,10 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  setPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -17,5 +21,16 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
 const auth = getAuth(app);
-
+setPersistence(auth, browserSessionPersistence)
+  .then(() => {
+    console.log(
+      "Configuración de persistencia de sesión cambiada correctamente",
+    );
+  })
+  .catch((error) => {
+    console.error(
+      "Error al cambiar la configuración de persistencia de sesión:",
+      error,
+    );
+  });
 export { app, auth, db };
